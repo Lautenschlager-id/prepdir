eb195cc1abbea83cf45650121a3101c17451905f
Lautenschlager <tai.romani@hotmail.com>
2021-12-27 15:48:56 -0300

    add spaces

--- libs/tokens.lua
+++ libs/tokens.lua
@@ -8,8 +8,8 @@ endOfLine = endOfLine .. "()"
 local tokenMatchers = { -- Other prefixes are defined in the class
 	IF     =            "IF "                .. expression .. endOfLine,
 	ELIF   =            "ELIF "              .. expression .. endOfLine,
-	ELSE   =            "ELSE"               .. endOfLine              ,
-	ENDIF  =            "ENDIF"              .. endOfLine              ,
+	ELSE   =            "ELSE"                             .. endOfLine,
+	ENDIF  =            "ENDIF"                            .. endOfLine,
 	DEFINE = prefix  .. "DEFINE (%a[%w_]+) " .. expression .. endOfLine,
 	_NEXT  =            "(%S+)"
 }
